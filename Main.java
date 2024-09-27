c30110319096d50537e15c2fb921a4d04da932c3
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-09-27 04:40:32 +0000

    Wrapper Class completed

--- Main.java
+++ Main.java
@@ -1,3 +1,33 @@
 public class Main {
+    public static void main(String[] args) {
+        // Integer Constructor
+        Integer number3 = new Integer(3);
+        System.out.println("The value of this Integer is: "+ number3);
 
+        // IntValue
+        int primitiveint;
+        primitiveint = number3.intValue();
+
+        // Double Value
+        Double doubleValue = new Double(12.34);
+        System.out.println("Double object created using double value: " + doubleValue);
+
+        // Max double value
+        System.out.println(Double.MAX_VALUE);
+
+        // Min double value
+        System.out.println(Double.MIN_VALUE);
+
+        // Autoboxing
+        Integer primitiveInt = 1;
+        Double primitiveDouble = 2.0;
+        System.out.println(primitiveInt.intValue());
+        System.out.println(primitiveDouble.doubleValue());
+
+        // Unboxing
+        Integer numInteger = new Integer(3);
+        int number = numInteger;
+        int sum = number + numInteger;
+        System.out.println("The sum is: "+sum);
+    }
 }
